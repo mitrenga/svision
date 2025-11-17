f20fb7dd430d8fedf444627fe503d386f8eb9d20
Libor Mitrenga <libor@mitrengovi.cz>
2025-11-17 20:31:16 +0100

    První funkční ovládání pomocí touch screen

--- js/inputEventsManager.js
+++ js/inputEventsManager.js
@@ -10,14 +10,15 @@ export class InputEventsManager {
   constructor(app) {
     this.app = app;
 
-    this.keysPressMap = { key: {} };
+    this.keysMap = {};
+    this.touchesMap = {};
     this.gamepads = {};
     this.blurWindow = false;
   } // constructor
-
+  
   eventKeyDown(event) {
-    if (this.app.model.autorepeatKeys || !(event.key in this.keysPressMap.key)) {
-      this.keysPressMap.key[event.key] = {press: 'Press', release: 'Release'};
+    if (this.app.model.autorepeatKeys || !(event.key in this.keysMap)) {
+      this.keysMap[event.key] = true;
       if (this.app.model) {
         if (!this.blurWindow) {
           this.app.model.sendEvent(0, {id: 'keyPress', key: event.key});
@@ -27,8 +28,8 @@ export class InputEventsManager {
   } // eventKeyDown
 
   eventKeyUp(event) {
-    if (event.key in this.keysPressMap.key) {
-      delete this.keysPressMap.key[event.key];
+    if (event.key in this.keysMap) {
+      delete this.keysMap[event.key];
     }
     if (this.app.model) {
       this.app.model.sendEvent(0, {id: 'keyRelease', key: event.key});
@@ -53,8 +54,8 @@ export class InputEventsManager {
     var buttons = event.buttons;
     for (var b = 0; b < 8; b++) {
       if (buttons%2 == 1) {
-        if (!('MouseButton'+(1<<b) in this.keysPressMap.key)) {
-          this.keysPressMap.key['MouseButton'+(1<<b)] = { press: 'Press', release: 'Release' };
+        if (!('MouseButton'+(1<<b) in this.keysMap)) {
+          this.keysMap['MouseButton'+(1<<b)] = true;
           if (this.app.model) {
             if (!this.blurWindow) {
               this.app.model.sendEvent(0, {id: 'keyPress', key: 'MouseButton'+(1<<b)});
@@ -70,8 +71,8 @@ export class InputEventsManager {
     var buttons = event.buttons;
     for (var b = 0; b < 8; b++) {
       if (buttons%2 == 0) {
-        if ('MouseButton'+(1<<b) in this.keysPressMap.key) {
-          delete this.keysPressMap.key['MouseButton'+(1<<b)];
+        if ('MouseButton'+(1<<b) in this.keysMap) {
+          delete this.keysMap['MouseButton'+(1<<b)];
           if (this.app.model) {
             this.app.model.sendEvent(0, {id: 'keyRelease', key: 'MouseButton'+(1<<b)});
           }
@@ -87,17 +88,48 @@ export class InputEventsManager {
   eventTouchStart(event) {
     event.preventDefault();
     if (this.app.model) {
-      this.app.model.sendEvent(0, {id: 'mouseClick', key: 'left', x: this.app.layout.convertClientCoordinateX(event.touches[0].clientX), y: this.app.layout.convertClientCoordinateY(event.touches[0].clientY)});
+      for (var t = 0; t < event.touches.length; t++) {
+        if (!(event.touches[t].identifier in this.touchesMap)) {
+          var touchX = this.app.layout.convertClientCoordinateX(event.touches[t].clientX);          
+          var touchY = this.app.layout.convertClientCoordinateY(event.touches[t].clientY);
+          var key = false;
+          if (touchX < this.app.model.borderEntity.width/2) {
+            key = 'TouchArea4';
+          }
+          if (touchX > this.app.model.borderEntity.width/2 && touchX < this.app.model.borderEntity.width/4*3) {
+            key = 'TouchArea1';
+          }
+          if (touchX > this.app.model.borderEntity.width/4*3) {
+            key = 'TouchArea2';
+          }
+          if (key !== false) {
+            this.app.model.sendEvent(0, {id: 'keyPress', key: key});
+            this.touchesMap[event.touches[t].identifier] = key;
+          }
+          this.app.model.sendEvent(0, {id: 'mouseClick', key: 'left', x: touchX, y: touchY});
+        }
+      }
     }
   } // eventTouchStart
 
   eventTouchEnd(event) {
+    if (this.app.model) {
+      Object.keys(this.touchesMap).forEach((identifier) => {
+        var validTouch = false;
+        for (var t = 0; t < event.touches.length; t++) {
+          if (identifier == event.touches[t].identifier) {
+            validTouch = true;
+            break;
+          }
+        }
+        if (!validTouch) {
+          this.app.model.sendEvent(0, {id: 'keyRelease', key: this.touchesMap[identifier]});
+          delete this.touchesMap[identifier];
+        }
+      });
+    }
     event.preventDefault();
   } // eventTouchEnd
-
-  eventTouchCancel(event) {
-    event.preventDefault();
-  } // eventTouchCancel
 
   eventTouchMove(event) {
     event.preventDefault();
@@ -119,7 +151,7 @@ export class InputEventsManager {
 
   eventBlurWindow(event) {
     this.blurWindow = true;
-    this.sendEventsActiveKeys('release');
+    this.sendEventsActiveKeys('Release');
     this.app.model.sendEvent(0, {id: 'blurWindow'});
   } // eventOnBlurWindow
 
@@ -128,11 +160,16 @@ export class InputEventsManager {
   } // eventOnFocusWindow
 
   sendEventsActiveKeys(type) {
-    Object.keys(this.keysPressMap).forEach((eventType) => {
-      Object.keys(this.keysPressMap[eventType]).forEach((key) => {
-        this.app.model.sendEvent(0, {id: eventType+this.keysPressMap[eventType][key][type], key: key});
-      });
+    Object.keys(this.keysMap).forEach((key) => {
+      this.app.model.sendEvent(0, {id: 'key'+type, key: key});
     });
+    Object.keys(this.touchesMap).forEach((identification) => {
+      this.app.model.sendEvent(0, {id: 'key'+type, key: this.touchesMap[identification]});
+    });
+    if (type == 'Release') {
+      this.keysMap = {};
+      this.touchesMap = {};
+    }
   } // sendEventsActiveKeys
 
 } // InputEventsManager
