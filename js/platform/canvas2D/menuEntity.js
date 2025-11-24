f6f15a84988d641036b300105b43db1c706de71f
Libor Mitrenga <libor@mitrengovi.cz>
2025-11-24 07:19:29 +0100

    MenuEntity podporuje parametry options

--- js/platform/canvas2D/menuEntity.js
+++ js/platform/canvas2D/menuEntity.js
@@ -9,41 +9,64 @@ import TextEntity from './textEntity.js';
 
 export class MenuEntity  extends AbstractEntity {
 
-  constructor(parentEntity, x, y, width, height, bkColor, dataSender, onGetData) {
+  constructor(parentEntity, x, y, width, height, bkColor, options, dataSender, onGetData) {
     super(parentEntity, x, y, width, height, false, bkColor);
     this.id = 'MenuEntity';
 
     this.dataSender = dataSender;
     this.onGetData = onGetData;
     this.selection = 0;
-    this.hoverColor = '#a9a9a9ff';
-    this.selectionHoverColor = this.app.platform.colorByName('blue');
     this.selectionEntity = null;
-    this.itemPenColor = this.app.platform.colorByName('blue');
-    this.selectionItemPenColor = this.app.platform.colorByName('brightWhite');
     this.menuEntities = [];
+
+    this.options = {
+      fonts: false,
+      leftMargin: 0,
+      rightMargin: 0,
+      topMargin: 0,
+      itemWidth: 0,
+      itemHeight: 0,
+      t1LeftMargin: 0,
+      t1TopMargin: 0, 
+      t2Width: 0,
+      t2RightMargin: 0,
+      t2TopMargin: 0, 
+      textColor: false,
+      selectionTextColor: false,
+      selectionBarColor: false,
+      hoverColor: false,
+      selectionHoverColor: false
+    };
+    Object.keys(options).forEach(key => {
+      if (key in this.options) {
+        this.options[key] = options[key];
+      }
+    });
+    if (this.options.itemWidth == 0) {
+      this.options.itemWidth = this.width-this.options.leftMargin-this.options.rightMargin;
+    }
   } // constructor
 
   init() {
     super.init();
 
-    this.selectionEntity = new AbstractEntity(this, 9, 8+this.selection*16, 210, 12, false, this.app.platform.colorByName('brightBlue'));
+    this.selectionEntity = new AbstractEntity(this, this.options.leftMargin, this.options.topMargin+this.selection*16, this.options.itemWidth, this.options.itemHeight, false, this.options.selectionBarColor);
     this.addEntity(this.selectionEntity);
 
     for (var y = 0; y < this.onGetData(this.dataSender, 'numberOfItems', 0); y++) {
-      var penColor = this.itemPenColor;
+      var penColor = this.options.textColor;
       if (y == this.selection) {
-        penColor = this.selectionItemPenColor;
+        penColor = this.options.selectionTextColor;
       }
       this.menuEntities[y] = [];
-      this.menuEntities[y][0] = new TextEntity(this, this.app.fonts.zxFonts8x8, 9, 8+y*16, 210, 12, this.onGetData(this.dataSender, 't1', y), penColor, false, {topMargin: 2, leftMargin: 3});
+      this.menuEntities[y][0] = new TextEntity(this, this.options.fonts, this.options.leftMargin, this.options.topMargin+y*16, this.options.itemWidth, this.options.itemHeight, this.onGetData(this.dataSender, 't1', y), penColor, false, {topMargin: this.options.t1TopMargin, leftMargin: this.options.t1LeftMargin});
       if (y != this.selection) {
-        this.menuEntities[y][0].hoverColor = this.hoverColor;
+        this.menuEntities[y][0].hoverColor = this.options.hoverColor;
       } else {
-        this.menuEntities[y][0].hoverColor = this.selectionHoverColor;
+        this.menuEntities[y][0].hoverColor = this.options.selectionHoverColor;
       }
       this.addEntity(this.menuEntities[y][0]);
-      this.menuEntities[y][1] = new TextEntity(this, this.app.fonts.zxFonts8x8, 105, 8+y*16, 114, 12, this.onGetData(this.dataSender, 't2', y), penColor, false, {topMargin: 2, rightMargin: 3, align: 'right'});
+      this.menuEntities[y][1] = new TextEntity(this, this.options.fonts, this.options.leftMargin+this.options.itemWidth-this.options.t2Width, this.options.topMargin+y*16, this.options.t2Width, this.options.itemHeight, this.onGetData(this.dataSender, 't2', y), penColor, false, {topMargin: this.options.t2TopMargin, rightMargin: this.options.t2RightMargin, align: 'right'});
       this.addEntity(this.menuEntities[y][1]);
     }
   } // init
@@ -59,14 +82,14 @@ export class MenuEntity  extends AbstractEntity {
     if (newSelection < 0 || newSelection >= this.menuEntities.length) {
       return;
     }
-    this.menuEntities[this.selection][0].hoverColor = this.hoverColor;
-    this.menuEntities[this.selection][0].setPenColor(this.itemPenColor);
-    this.menuEntities[this.selection][1].setPenColor(this.itemPenColor);
+    this.menuEntities[this.selection][0].hoverColor = this.options.hoverColor;
+    this.menuEntities[this.selection][0].setPenColor(this.options.textColor);
+    this.menuEntities[this.selection][1].setPenColor(this.options.textColor);
     this.selection = newSelection;
-    this.menuEntities[this.selection][0].hoverColor = this.selectionHoverColor;
-    this.menuEntities[this.selection][0].setPenColor(this.selectionItemPenColor);
-    this.menuEntities[this.selection][1].setPenColor(this.selectionItemPenColor);
-    this.selectionEntity.y = 8+this.selection*16;
+    this.menuEntities[this.selection][0].hoverColor = this.options.selectionHoverColor;
+    this.menuEntities[this.selection][0].setPenColor(this.options.selectionTextColor);
+    this.menuEntities[this.selection][1].setPenColor(this.options.selectionTextColor);
+    this.selectionEntity.y = this.options.topMargin+this.selection*16;
   } // changeMenuItem
   
   handleEvent(event) {
